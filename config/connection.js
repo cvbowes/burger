12fbc53495ab4fd40055923838728178f5a4f5fb
Caroline Bowes <carvbowes@gmail.com>
2017-08-28 21:40:25 -0400

    jawsdb check

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+	console.log(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		port: 3306,
